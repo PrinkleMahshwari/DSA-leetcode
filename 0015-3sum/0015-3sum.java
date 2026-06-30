722cf620e229c6bbebe6bf31cd72757ac18309ed
PrinkleMaheshwari <prinklekellabe1st@gmail.com>
2026-06-30 23:31:10 +0500

    Time: 33 ms (73.83%), Space: 59.1 MB (59.71%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        // store final list of distinct triplets
+        List<List<Integer>> result = new ArrayList<>();
+
+        // sort the array to use two pointers techinque and skip duplicates easily
+        Arrays.sort(nums);
+
+        // traverse every number to fix the first element of the triplet
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // skip duplicate values for the first element of the triplet
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            
+            // track the remaining two elements using two pointers
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            // look for pairs that sum up to target value
+            while (left < right) {
+
+                // calcualte current sum of the triplet
+                int sum = nums[i] + nums[left] + nums[right];
+
+                // triplet found
+                if (sum == 0) {
+
+                    // add valid triplet to the result list
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // skip duplicate values for the second
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    
+                    //skip duplicate values for third element
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+                    
+                    // move both pointers
+                    left++;
+                    right--;
+                }
+                // sum is too small, move left pointer to increase sum
+                else if (sum < 0) {
+                    left++;
+                }
+                // sum is too large, move right pointer to decrease sum
+                else {
+                    right--;
+                }
+            }
+        }
+
+        // return final list of triplets
+        return result;
+    }
+}
