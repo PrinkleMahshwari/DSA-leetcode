a69a03635bd4517429344db1969e26dcdaa2cc1a
PrinkleMaheshwari <prinklekellabe1st@gmail.com>
2026-06-30 21:17:17 +0500

    Time: 10 ms (95.55%), Space: 46.2 MB (78.62%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,37 +1,43 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         
-        int n = s.length();
+        // store the last seen index of characters a, b, and c
+        int lastA = -1;
+        int lastB = -1;
+        int lastC = -1;
 
-        // frequency array for a, b, c
-        int[] freq = new int[3];
+        // store final count of substrings
+        int result = 0;
 
-        int left = 0;
-        int right = 0;
+        // traverse every character in the string
+        for (int i = 0; i < s.length(); i++) {
 
-        int count = 0;
+            // get current character
+            char current = s.charAt(i);
 
-        // expand window using right pointer
-        while (right < n) {
-
-            // include current character in window
-            freq[s.charAt(right) - 'a']++;
-
-            // check if window is valid (contain a, b, c)
-            while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
-
-                // all substrings starting from left and ending from right to n-1 are valid
-                count += (n - right);
-
-                // shrink window form left
-                freq[s.charAt(left) - 'a']--;
-                left++;
+            // update the last seen index for the current character
+            if (current == 'a') {
+                lastA = i;
+            }
+            else if (current == 'b') {
+                lastB = i;
+            }
+            else if (current == 'c') {
+                lastC = i;
             }
 
-            // expand window
-            right++;
-        }
+            // check whether all three characters have been seen at least once
+            if (lastA != -1 && lastB != -1 && lastC != -1) {
 
-        return count;
+                // find the smallest index among the last seen positions
+                int minIndex = Math.min(lastA, Math.min(lastB, lastC));
+
+                // valid substrings can start anywhere from index 0 up to minIndex
+                result += minIndex + 1;
+            }
+        }
+        
+        // return final count
+        return result;
     }
 }
