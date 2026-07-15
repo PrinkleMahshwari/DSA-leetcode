218761708d3f5496df90473967197f1d85fe2db0
PrinkleMaheshwari <prinklekellabe1st@gmail.com>
2026-07-15 20:40:28 +0500

    Time: 1 ms (83.11%), Space: 42.3 MB (93.05%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,5 +1,7 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
+        // sumOdd = n*n, sumEven = n*(n+1)
+        // gcd(n*n, n*(n+1)) = n * gcd(n, n+1) = n * 1, since consecutive integers are always coprime
         return n;
     }
 }
