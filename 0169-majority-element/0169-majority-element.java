76bf2f49148d22ac6a9ae0db221e778b39d9516b
PrinkleMaheshwari <prinklekellabe1st@gmail.com>
2026-07-19 20:04:39 +0500

    Time: 1 ms (99.9%), Space: 55.8 MB (32%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        
-        // Boyer-Moore voting: candidate cancels out against every non-matching
-        // element. since a true majority element appears more than n/2 times,
-        // it can never be fully cancelled out, so whatever survives is the answer
-        int candidate = nums[0];
-        int count = 0;
 
-        for (int num : nums) {
-            if (count == 0)
+        int candidate = nums[0];
+        int count = 1; // start at 1 since candidate is nums[0] itself, skip index 0 in the loop
+
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+
+            if (count == 0) {
                 candidate = num;
-            
-            count += (num == candidate) ? 1 : -1;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
         }
 
         return candidate;
