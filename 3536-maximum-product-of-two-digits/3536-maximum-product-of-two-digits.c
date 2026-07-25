0d54d4dfb29f6853a7ee59e48e50aff2ad013aa6
PrinkleMaheshwari <prinklekellabe1st@gmail.com>
2026-07-25 20:00:59 +0500

    Time: 0 ms (100%), Space: 9.1 MB (83.08%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.c
@@ -0,0 +1,18 @@
+int maxProduct(int n) {
+    int max1 = 0; // tracks the largest digit
+    int max2 = 0; // tracks the second largest digit
+
+    while (n > 0) {
+        int digit = n % 10; // extract the last digit
+        n /= 10; // remove the last digit
+
+        if (digit > max1) {
+            max2 = max1; //the old largest becomes the second largest
+            max1 = digit; // update new largest
+        } else if (digit > max2) {
+            max2 = digit; // update the second largest
+        }
+    }
+
+    return max1 * max2;
+}
