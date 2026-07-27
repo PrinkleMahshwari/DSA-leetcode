7f66efc8dee7e8a3eb038068244cfeac8740387c
PrinkleMaheshwari <prinklekellabe1st@gmail.com>
2026-07-27 15:13:49 +0500

    Time: 0 ms (100%), Space: 9.1 MB (13.27%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.c
@@ -0,0 +1,16 @@
+int maxProduct(int* nums, int numsSize) {
+    int max1 = 0;
+    int max2 = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        int num = nums[i];
+        if (num > max1) {
+            max2 = max1;
+            max1 = num;
+        } else if (num > max2) {
+            max2 = num;
+        }
+    }
+
+    return (max1 - 1) * (max2 - 1);
+}
