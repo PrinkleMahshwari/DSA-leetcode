c11430025f71ccbd28b8e2d5bb6c3b31db3b9fa2
PrinkleMaheshwari <prinklekellabe1st@gmail.com>
2026-08-02 18:36:21 +0500

    Time: 5 ms (23.31%), Space: 42.8 MB (95.35%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,6 +1,23 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        // Alice can always win by choosing all odd or all even indexed piles greedily
-        return true;
+        int n = piles.length;
+        // this is 1D array since we only need previous diagonal
+        int[] dp = new int[n];
+
+        // this is base case: single piles
+        for (int i = 0; i < n; i++) {
+            dp[i] = piles[i];
+        }
+
+        // this is filling by length, updating dp in-place
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                // this is picking best of left or right end
+                dp[i] = Math.max(piles[i] - dp[i + 1], piles[j] - dp[i]);
+            }
+        }
+
+        return dp[0] > 0;
     }
 }
