57e776405224f40e4d2970b25b42a10ec0226739
PrinkleMaheshwari <prinklekellabe1st@gmail.com>
2026-08-24 16:19:39 +0500

    Time: 61 ms (88.89%), Space: 70.2 MB (11.11%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var stoneGameVIII = function(stones) {
+    const n = stones.length;
+
+    // Use BigInt or flat structures to prevent number truncation on long ranges
+    const prefix = new Float64Array(n);
+    prefix[0] = stones[0];
+
+    for (let i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] + stones[i];
+    }
+
+    let ans = prefix[n - 1];
+
+    for (let i = n - 2; i >= 1; i--) {
+        ans = Math.max(ans, prefix[i] - ans);
+    }
+
+    return ans;
+};
