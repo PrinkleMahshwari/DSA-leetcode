b0c9f0135bfdbaae5086269437116d56cf1edbef
PrinkleMaheshwari <prinklekellabe1st@gmail.com>
2026-09-08 11:20:27 +0500

    Time: 1 ms (83.65%), Space: 42.4 MB (85.26%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -2,7 +2,6 @@ class Solution {
     public int countCommas(int n) {
         // numbers from 1000 to 100000 --> 1 comma return n - 999
         // 1 to 999 contain no comma ---> return 0
-        if (n < 1000) return 0;
-        return n - 999;
+        return (n < 1000) ? 0 : n - 999;
     }
 }
